b35913411375d085eb5db32bdf1c6454319364f2
Solcis90 <126728514+Solcis90@users.noreply.github.com>
2023-05-19 20:54:39 -0300

    Add files via upload

---
+++ examen19_5.py
@@ -0,0 +1,37 @@
+#1)
+# def Suma_Infinita():
+#     return Suma_Infinita
+#     while contar > 100000000:
+#         sum = contar + 1
+#     print(f'{contar}numero contador')
+#     contar +=1
+#     print(f"la suma de los argumentos es de {sum}")
+
+
+
+#2)
+# def Formula_peso(peso, altura):
+#     return Formula_peso
+#     Formula_peso = peso / (altura ** 2)
+#     peso = int(input("Ingrese su peso: "))
+#     altura = int(input("Ingrese su altura: "))
+#     resultado:Formula_peso(peso, altura)
+#     Print(resultado)
+
+#3)
+# def numeros():
+#     return numeros
+#     for numeros in range (100):
+#         if numeros * 3:
+#             return "Fizz"
+#         if numeros * 5:
+#             return "Buzz"
+#         elif numeros * 3 and numeros * 5:
+#             return "FizzBuzz"
+
+#     resultado:numeros
+#     print(resultado)
+
+
+    
+    
